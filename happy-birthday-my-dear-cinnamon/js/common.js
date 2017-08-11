72536a7387971ddca53188d64189dedf09064647
BonoBelarus <bono.ex@gmail.com>
2017-08-12 02:56:08 +0300

    1

---
+++ happy-birthday-my-dear-cinnamon/js/common.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+	$('#fullpage').fullpage();
+
+	function run() {
+		var image = document.getElementById('background');
+		image.onload = function() {
+			var engine = new RainyDay({
+				image: this
+			});
+			engine.rain([ [1, 2, 8000] ]);
+			engine.rain([ [3, 3, 0.88], [5, 5, 0.9], [6, 2, 1] ], 100);
+		};
+		image.crossOrigin = 'anonymous';
+		image.src = 'http://i.imgur.com/N7ETzFO.jpg';
+	}
+});
